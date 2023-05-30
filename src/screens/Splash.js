2ee7b997e7a56c9c7d5e9606337e5169f6d0ece9
sushmali1230 <msushant1230@gmail.com>
2023-05-30 07:51:38 +0530

    Local Storage Permission Granted

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -7,31 +7,42 @@ const Splash = () => {
 
     const navigation = useNavigation();
     useEffect(() => {
-        setTimeout(() => {
-            navigation.navigate('Dashboard')
-        }, 2000)
+        request_runtime_permission();
     }, []);
 
-    const requestStoreagePermission = async () => {
+    const request_runtime_permission = async () => {
         try {
             const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                {
+                'title': 'ReactNativeCode Write Permission',
+                'message': 'ReactNativeCode App needs access to write data '
+                }
+            );
+            const granted1 = await PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
                 {
-                    title: 'Movie App Storage Permission',
-                    message:
-                        'Movie App needs access to your storage '
-                },
+                'title': 'ReactNativeCode Read Permission',
+                'message': 'ReactNativeCode App needs access to read data '
+                }
             );
             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                console.log('You can use the storage');
-                
-            } else {
-                console.log('Storage permission denied');
+                if (granted1 === PermissionsAndroid.RESULTS.GRANTED) {
+                    setTimeout(() => {
+                        navigation.navigate('Dashboard')
+                    }, 2000)
+                }
+                else {
+                    console.log("Storage Permission Not Granted");
+                }   
+            }
+            else {
+                console.log("Camera Permission Not Granted");
             }
         } catch (err) {
-            console.warn(err);
+            console.warn(err)
         }
-    };
+    }
 
     return (
         <View>
